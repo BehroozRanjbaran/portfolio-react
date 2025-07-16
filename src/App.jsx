159a806b281d77e3d1a0d7911d1da1bf4d9e3fbd
Behrouz Ranjbaran <ranjbaran.behrouz@gmail.com>
2025-07-16 14:41:13 +0330

    assets's folder and It's subfolders added to project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import styles from './App.module.css';
+
+function App() {
+
+
+  return (
+    <>
+      <div className={styles.App}>My Portfolio</div>
+       
+    </>
+  )
+}
+
+export default App
