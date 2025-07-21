9e07ce43d64fe1c3852567fb632a3ae183514e6c
Behrouz Ranjbaran <ranjbaran.behrouz@gmail.com>
2025-07-21 21:48:08 +0330

    module css and Navbar component created

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 
 import styles from './App.module.css';
+import { Navbar } from './components/Navbar/Navbar';
 
 function App() {
 
 
   return (
     <>
-      <div className={styles.App}>My Portfolio</div>
+      <div className={styles.App}>
+
+      <Navbar/>
+
+      </div>
        
     </>
   )
