dd953ff88014b499958eac80b49f3a79b2c8ddcb
Behrouz Ranjbaran <ranjbaran.behrouz@gmail.com>
2025-07-26 15:01:03 +0330

    some changes in hero and navbar codes

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,16 @@
-
-import styles from './App.module.css';
-import Hero from './components/Hero/Hero';
-import { Navbar } from './components/Navbar/Navbar';
+import styles from "./App.module.css";
+import {Hero} from "./components/Hero/Hero";
+import { Navbar } from "./components/Navbar/Navbar";
 
 function App() {
-
-
   return (
     <>
       <div className={styles.App}>
-
-      <Navbar/>
-      <Hero />
+        <Navbar />
+        <Hero />
       </div>
-       
     </>
-  )
+  );
 }
 
-export default App
+export default App;
