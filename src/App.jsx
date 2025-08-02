2168edb2077e57c1744ae157b0d4029914063e7c
Behrouz Ranjbaran <ranjbaran.behrouz@gmail.com>
2025-08-02 13:04:19 +0330

    Added About section and styles to project

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import styles from "./App.module.css";
 import { Navbar } from "./components/Navbar/Navbar";
+import { Hero } from "./components/Hero/Hero";
+import { About } from "./components/About/About";
 
 import "./App.css";
 
@@ -7,6 +9,8 @@ function App() {
   return (
     <div className={styles.App}>
       <Navbar />
+      <Hero />
+      <About />
     </div>
   );
 }
--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,9 @@
 export const getImageUrl = (path) => {
   return `/assets/${path}`;
 };
+
+// I know something about below code and then change that to above code
+
+// export const getImageUrl = (path) => {
+//   return new URL(`/assets/${path}`, import.meta.url).href;
+// };
